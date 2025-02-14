64ffec456ed7da86a531ba55920ec71140250548
Doniyorjon7399 <zokirovdoniyorjon2001@gmail.com>
2025-02-14 20:38:24 +0500

    exam 5-oy v-2

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,9 @@
+import moment from "moment";
 export default {
   ifequal: function (a, b, options) {
     return a == b ? options.fn(this) : options.inverse(this);
+  },
+  formatDate(data) {
+    return moment(data).format("DD MM, YYYY");
   },
 };
